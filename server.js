7b83f20c8804e2c15c9634b231ab46cb1e6f4cc8
salody <335923574@qq.com>
2019-01-30 17:42:21 +0800

    修复webpack的问题

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require('http')
+/* eslint-disable no-console,node/no-extraneous-require */
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
 
-const hostname = '127.0.0.1'
-const port = 3000
+// serve static assets normally
+app.use(express.static(__dirname + '/public'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 201
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({
-    msg: 'hello world'
-  }))
+// Handles all routes so you do not get a not found error
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+app.listen(port)
+console.log("server started on port " + port)
